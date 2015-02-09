906863dd2c8babf21ea9db7cf9419efd4a58f544
John Martirano <johnmartirano@gmail.com>
2015-02-09 00:07:46 -0500

    testing

--- app/models/container.rb
+++ app/models/container.rb
@@ -4,5 +4,9 @@ class Container < ActiveRecord::Base
   has_many :thing1s, through: :container_things, source: :thingable, source_type: "Thing1"
   has_many :thing2s, through: :container_things, source: :thingable, source_type: "Thing2"
   has_many :thing3s, through: :container_things, source: :thingable, source_type: "Thing3"
+
+  def thingables
+    return thing1s + thing2s + thing3s
+  end
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,38 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-container_a = Container.create( name: "ContainerA" )
+containers = []
+containers << Container.create( name: "ContainerA" ) unless Container.find_by_name("ContainerA")
+containers << Container.create( name: "ContainerB" ) unless Container.find_by_name("ContainerB")
+containers << Container.create( name: "ContainerC" ) unless Container.find_by_name("ContainerC")
 
 thing1_attrs = []
 (1..5000).each do |i|
-  thing1_attrs << { name: "#{i}one", feature1: "one#{i}", container_thing_attributes: { name: "#{i}one", container_id: container_a.id, thingable_type: "Thing1" } }
+  thing1_attrs << { name: "#{i}one", feature1: "one#{i}", container_thing_attributes: { name: "#{i}one", container_id: containers.sample.id, thingable_type: "Thing1" } }
 end
 
 thing2_attrs = []
 (1..5000).each do |i|
-  thing2_attrs << { name: "#{i}two", feature2: "two#{i}", container_thing_attributes: { name: "#{i}two", container_id: container_a.id, thingable_type: "Thing2" } }
+  thing2_attrs << { name: "#{i}two", feature2: "two#{i}", container_thing_attributes: { name: "#{i}two", container_id: containers.sample.id, thingable_type: "Thing2" } }
 end
 
 thing3_attrs = []
 (1..5000).each do |i|
-  thing3_attrs << { name: "#{i}three", feature3: "three#{i}", container_thing_attributes: { name: "#{i}three", container_id: container_a.id, thingable_type: "Thing3" } }
+  thing3_attrs << { name: "#{i}three", feature3: "three#{i}", container_thing_attributes: { name: "#{i}three", container_id: containers.sample.id, thingable_type: "Thing3" } }
 end
 
 puts "creating thing1s"
 thing1_attrs.each do |attrs|
-  Thing1.create(attrs)
+  Thing1.create(attrs) unless Thing1.find_by_name(attrs[:name])
 end
 
 puts "creating thing2s"
 thing2_attrs.each do |attrs|
-  Thing2.create(attrs)
+  Thing2.create(attrs) unless Thing2.find_by_name(attrs[:name])
 end
 
 puts "creating thing3s"
 thing3_attrs.each do |attrs|
-  Thing3.create(attrs)
+  Thing3.create(attrs) unless Thing3.find_by_name(attrs[:name])
 end
 
